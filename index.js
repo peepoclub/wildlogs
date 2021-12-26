ce58e319c0deb95c73e7e25ef53560be650ba405
Sergey <master@peepo.club>
2021-12-26 10:52:38 +0700

    move to pnpm, update dependencies

--- index.js
+++ index.js
@@ -8,23 +8,33 @@ import { ChatClient } from "dank-twitch-irc";
 const client = new ChatClient();
 
 client.on("JOIN", (e) => console.log(`🟢 Connected to chat ${e.channelName}`));
+
 client.on("close", (e) => {
   if (e !== null) {
     console.error("🔴 Client closed due to error", e);
   }
 });
 
-client.on("PRIVMSG", async (e) => {
+client.on("PRIVMSG", async (data) => {
+  const {
+    channelName: channel,
+    channelID,
+    senderUsername: name,
+    displayName,
+    senderUserID: userID,
+    messageText: message,
+  } = data;
+
   // Add new line to log database
   await prisma.logs.create({
     data: {
-      channel: e.channelName,
-      channelID: e.ircTags["room-id"],
-      name: e.ircPrefix.nickname,
-      displayName: e.displayName,
-      userID: e.ircTags["user-id"],
-      message: e.messageText,
-      tags: JSON.stringify(e.ircTags),
+      channel,
+      channelID,
+      name,
+      displayName,
+      userID,
+      message,
+      tags: JSON.stringify(data.ircTags),
       timestamp: (Date.now() / 1000) | 0,
     },
   });
